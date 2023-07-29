2582deeee854661fc55621154bb7bf448a07fda8
Grigori Kaplin <82471308+LittleBrotherProg@users.noreply.github.com>
2023-07-29 13:18:24 +0300

    iterator. finisf task_2 and task_3

--- Professional work with Python/Iterator/task_1.py
+++ Professional work with Python/Iterator/task_1.py
@@ -2,37 +2,30 @@ class FlatIterator:
 
     def __init__(self, list_of_list):
         self.l_of_l = list_of_list
-
-    def __iter__(self):
         self.my_list = []
         self.all_list = len(self.l_of_l)
         self.count_all_list = 0
         self.count_list = 0
+
+    def __iter__(self):
+        print('Старт цикла')
         return self
 
     def __next__(self):
         if self.count_all_list != self.all_list:
             self.items = len(self.l_of_l[self.count_all_list])
-            if self.count_list != self.items:
-                self.item = self.l_of_l[self.count_all_list][self.count_list]
-                self.my_list.append(self.item)
-                self.count_list += 1
-                return self.my_list[len(self.my_list)-1]
-            else:
+            if self.count_list == self.items:
                 self.count_all_list += 1
-                if self.count_all_list == self.all_list and self.count_list == self.items:
+                if self.count_all_list == self.all_list:
+                    print('Конец цикла')
                     raise StopIteration
                 else:
-                    self.count_list = 0
-                    self.items = len(self.l_of_l[self.count_all_list])
-                    if self.count_list != self.items:
-                        self.item = self.l_of_l[self.count_all_list][self.count_list]
-                        self.my_list.append(self.item)
-                        self.count_list += 1
-                        return self.my_list[len(self.my_list)-1]
-        else:
-            print('цикл завершён')
-            return self.my_list
+                    self.count_list = 0    
+            self.item = self.l_of_l[self.count_all_list][self.count_list]
+            self.my_list.append(self.item)
+            self.count_list += 1
+            return self.item
+                
 
             
 
