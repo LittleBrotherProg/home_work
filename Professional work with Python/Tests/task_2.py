6fd9fafe977c67e18af0f3609d2336a284cd6a43
Grigori Kaplin <82471308+LittleBrotherProg@users.noreply.github.com>
2023-08-01 01:45:30 +0300

    Test. update task_2

--- Professional work with Python/Tests/task_2.py
+++ Professional work with Python/Tests/task_2.py
@@ -4,37 +4,48 @@ import os
 
 class API_YA:
 
-    def __init__(self, name):
-        self.name_folder = name
+    def __init__(self):
         self.base_url = 'https://cloud-api.yandex.net/v1/disk/'
         self.token = os.getenv('YAME')
-        # self.authorization = {'Authorization': self.token}
         self.headers = {
-                        'Content-Type': 'application/json', 
                         'Authorization': self.token
                         }
-        self.params = 'disk:/'
+        self.params = {"path":'disk:/'}
     
 
     def status_code(self, *args):
         return  args[1](args[0], headers=self.headers, params = args[2]).status_code
         
 
-    def create_folder(self):
+    def create_folder(self, name):
         status = self.status_code(self.base_url, 
                                   requests.get, 
                                   self.params
                                   )
-        
+
         if status == 200:
-            print(self.status_code(self.base_url + 'resources', 
-                                   requests.put, 
-                                   'disk:/' + self.name_folder
-                                   ))
+            self.status_code(self.base_url + 'resources', 
+                             requests.put,
+                             {"path": self.params["path"] + self.name_folder}
+                                )
              
 
 
 
-if __name__ == '__main__':
-    ya = API_YA("test")
-    ya.create_folder()
+# if __name__ == '__main__':
+#     ya = API_YA(name_folder = "test")
+#     ya.create_folder()
+
+@pytest.fixture
+def preparation():
+    ya = API_YA()
+    ya.create_folder(name_folder = "test")
+
+def test_yandex_disk_access():
+    ya = API_YA()
+    assert 200 == ya.status_code('https://cloud-api.yandex.net/v1/disk/', 
+                                  requests.get, 
+                                  {"path":'disk:/'})
+    assert 400 != ya.status_code('https://cloud-api.yandex.net/v1/disk/', 
+                                  requests.get, 
+                                  {"path":'disk:/'})
